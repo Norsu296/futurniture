028ed0e6db501f05084dceb43cb29cf5e074627a
Jakub Brózda <brozdajakub@gmail.com>
2021-09-30 19:38:27 +0200

    Przygotowanie projektu

---
+++ src/main/java/pl/kuba/futurniture/FuturnitureApplication.java
@@ -0,0 +1,13 @@
+package pl.kuba.futurniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturnitureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuturnitureApplication.class, args);
+    }
+
+}
